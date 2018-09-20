413f89a884c58a28734945297ee46f96a4901733
IceHax <vecchiotom1@gmail.com>
2018-09-20 15:19:27 +0200

    tests with react and next

--- snail-server/pages/Stats.js
+++ snail-server/pages/Stats.js
@@ -1,9 +1,25 @@
 import React from 'react'
+const mongoose = require('mongoose')
+import { isAuthenticated } from '../components/auth.js'
 
 //TODO: start writing some react thingys maybe? :D
 
-export default () => (
-	<div className="main-content">
-		<p>placeholder</p>
-	</div>
-)
+export default class extends React.Component{
+	static async getInitialProps(context) {
+		if (context.req && context.req.headers && !isAuthenticated(context.req)) {
+			context.req.res.redirect('/login')
+		}
+		this.userlicense=context.req.user.license
+		return {}
+	}
+
+	render(){
+		const license = this.license
+		mongoose.model('User').findOne({license},(err, result)=>{
+			<div className="main-content">
+				<p>placeholder, but i know your email is: {result.email} </p>
+			</div>
+		})
+
+	}
+}
