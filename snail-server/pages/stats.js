5ef9e48440e327c98b1c5c4725167971a0a77f2c
IceHax <vecchiotom1@gmail.com>
2018-09-24 19:39:11 +0200

    fixy fix

--- snail-server/pages/stats.js
+++ snail-server/pages/stats.js
@@ -2,19 +2,19 @@ import React from 'react'
 import Router from 'next/router'
 
 export default class extends React.Component{
-    constructor(props, context){
-        super(props, context)
-    }
+	constructor(props, context){
+		super(props, context)
+	}
 
-    componentDidMount(){
-        if (!this.props.authenticated) {
-            Router.push("/login")
-        } 
-    }
+	componentDidMount(){
+		if (!this.props.authenticated) {
+			Router.push('/login')
+		} 
+	}
 
-    render() {
-         return <div className="main-content">	
-				<p>a placeholder, i guess</p>	
+	render() {
+		return <div className="main-content">	
+			<p>a placeholder, i guess</p>	
 		</div>
-    }
+	}
 }
